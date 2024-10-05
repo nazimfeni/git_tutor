90026d1fc2f76ec5eb4746cc0687e29940f34b78
nazimjrf <nazim@jrfintel.com>
2024-10-05 16:02:50 +0600

    first change

--- about.php
+++ about.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
 </head>
+
 <body>
       <h1>About Page</h1>
       <p>I am developer</p>
@@ -14,8 +16,9 @@
       <p>I like Python</p>
       <p>I don't like html</p>
       <p> Change</p>
-      <p> I don't like Java.</p>
-       <p> I don't like Kotlin.</p>
+      <p> Change 1</p>
+      <p> I don't like Kotlin.</p>
 
 </body>
+
 </html>
