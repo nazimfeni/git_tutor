746dfa90a9539dab0faf2b7993bf6b095ad09c09
nazimjrf <nazim@jrfintel.com>
2024-10-05 16:06:33 +0600

    updated old commid

--- about.php
+++ about.php
@@ -1,10 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Document</title>
 </head>
+
 <body>
       <h1>About Page</h1>
       <p>I am developer</p>
@@ -15,7 +17,8 @@
       <p>I don't like html</p>
       <p> Change</p>
       <p> I don't like Java.</p>
-       <p> I don't like Kotlin.</p>
+      <p> I don't like Kotlin.....</p>
 
 </body>
+
 </html>
